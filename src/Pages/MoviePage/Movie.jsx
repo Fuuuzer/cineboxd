4bed4dafcedc4d80e767c988ffb0bf2f15026583
Joao Fuzer <fuuzer_@hotmail.com>
2025-11-25 10:03:45 -0300

    finished loading state

--- src/Pages/MoviePage/Movie.jsx
+++ src/Pages/MoviePage/Movie.jsx
@@ -8,40 +8,55 @@ import './Movie.css'
 const Movie = () => {
   const [movie, setMovie] = React.useState([]);
   const [movieCasting, setMovieCasting] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
   const apiKey = import.meta.env.VITE_API_KEY;
 
   const { id } = useParams();
 
   React.useEffect(() => {
+    setIsLoading(true);
+
     const getMovie = async () => {
       const movieData = await fetchMovieById(apiKey, id);
+      console.log(movieData)
       setMovie(movieData)
+      setIsLoading(false);
     }
 
     const getCasting = async () => {
       const castingData = await fetchCasting(apiKey, id);
       console.log(castingData)
       setMovieCasting(castingData)
+      setIsLoading(false);
     }
 
     getMovie();
     getCasting()
-  }, [id])
+  }, [id]);
 
 
   return (
-    <div className='movie'>
-      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path || movie.backdrop_path}`} alt={movie.title} />
-      <div className='movie-infos'>
-        <h2>{movie.title}</h2>
-        <p><strong>Data de Lançamento: </strong>{movie.release_date}</p>
-        <p className='movie-sinopse'>{movie.overview}</p>
-        {movieCasting.map((casting) => {
-          return <p>{casting.name}</p>
-        })}
-      </div>
-    </div>
+    <>
+      {isLoading ? (<p className='loading'>Loading...</p >) : (
+        <div className='movie'> <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path || movie.backdrop_path}`} alt={movie.title} />
+          <div className='movie-infos'>
+            <h2>{movie.title}</h2>
+            <p><strong>Duração </strong>{movie.runtime} min</p>
+            <p><strong>Data de Lançamento: </strong>{movie.release_date}</p>
+            <p className='movie-sinopse'>{movie.overview}</p>
+            <div className='casting'>
+              <h3>Casting</h3>
+              <div>
+                {movieCasting.map((casting) => {
+                  return <p key={casting.name}>{casting.name}</p>
+                })}
+              </div>
+            </div>
+          </div>
+        </div>)}
+    </>
   )
 }
+
 
 export default Movie
