8a7ab98a1fb924ef1e1dc21f22be0c899e15659c
Joao Fuzer <fuuzer_@hotmail.com>
2026-01-24 15:49:20 -0300

    added link to movie

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { fetchPopularMovies } from '../../services/api';
 import './Movies.css'
+import { Link } from 'react-router';
 
 const Movies = ({ apikey }) => {
   const [movies, setMovies] = React.useState([]);
@@ -12,14 +13,15 @@ const Movies = ({ apikey }) => {
     }
 
     getMovies()
-  }, [apikey])
+  }, [apikey]);
 
   return (
+
     <div className='movies-container'>
       {movies.map((movie) => (
-        <div className='movies' key={movie.id}>
+        <Link to={`./${movie.id}`} className='movies' key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path || movie.backdrop_path}`} alt={movie.title} />
-        </div>
+        </Link>
       ))}
     </div>
   )
